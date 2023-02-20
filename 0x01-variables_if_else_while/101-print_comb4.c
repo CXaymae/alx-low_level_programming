b5f4e55a03f2adc6b5e78805652a3d7c6ae59e10
CXaymae <morabiitchaima@gmail.com>
2023-02-20 11:59:21 -0800

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,7 @@
  * main - Entry point
  * Numbers must be separated by ,, followed by a space
  * The three digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits
+ * 012, 120, are considered the same combination of the three digits
  * Print only the smallest combination of three digits
  * Numbers should be printed in ascending order, with three digits
  * You can only use the putchar function (every other function is forbidden)
